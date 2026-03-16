b32615e8ede4c60bea2de6688a7804baf5efb3b6
V.JAGADEESH <197778704+jagadeesh-431@users.noreply.github.com>
2026-03-16 22:00:31 +0530

    added cmd line

--- Day01_Tasks/missing_num_inArr.java
+++ Day01_Tasks/missing_num_inArr.java
@@ -1,7 +1,7 @@
 public class missing_num_inArr{
 public static void main(String[] args) {
         int arr[] = {1,2,3,4,6,7,8};
-        int n = arr.length + 1; // n+1
+        int n = arr.length + 1; // n+1 coz 1 value is missing
         int sum = 0;
         int hint = n * (n + 1) / 2; 
         for(int i=0;i<arr.length;i++){
